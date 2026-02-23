aa322b7dd0af075b9a198e4d83764ac607f54a12
RahmanIslamIen <rahmanislamien@gmail.com>
2026-02-23 16:51:49 +0700

    ganti ke hono dan pake sepenuh nya npm

--- generated/prismabox/barrel.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Role";
-export * from "./User";
-export * from "./Post";
-export * from "./Category";
-export * from "./Comment";
-export * from "./__nullable__";
-export * from "./__transformDate__";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { Elysia } from "elysia";
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Hono()
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
